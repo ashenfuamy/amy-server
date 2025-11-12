68676bfe3242ee96e0eaf0b9c5529af2d8927274
Liu Yuanshen <52831297+ashenfuamy@users.noreply.github.com>
2025-11-12 18:29:08 +0800

    import hutool

---
+++ src/main/java/site/ashenstation/amyserver/config/mybatisFlex/SimpleUUIDKeyGenerator.java
@@ -0,0 +1,10 @@
+package site.ashenstation.amyserver.config.mybatisFlex;
+
+import com.mybatisflex.core.keygen.IKeyGenerator;
+
+public class SimpleUUIDKeyGenerator implements IKeyGenerator {
+    @Override
+    public Object generate(Object o, String s) {
+        return null;
+    }
+}
---
+++ src/test/java/site/ashenstation/amyserver/MybatisFlexTestApplicationTests.java
@@ -0,0 +1,26 @@
+package site.ashenstation.amyserver;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import site.ashenstation.amyserver.entity.MdaTagPo;
+import site.ashenstation.amyserver.entity.table.MdaTagPoTableDef;
+import site.ashenstation.amyserver.mapper.MdaTagMapper;
+
+@SpringBootTest
+public class MybatisFlexTestApplicationTests {
+
+    @Autowired
+    private MdaTagMapper mdaTagMapper;
+    @Test
+    void contextLoads() {
+        QueryWrapper where = QueryWrapper.create()
+                .select()
+                .where(MdaTagPoTableDef.MDA_TAG_PO.ID.eq("100224"));
+
+        MdaTagPo mdaTagPo = mdaTagMapper.selectOneByQuery(where);
+
+        System.out.println(mdaTagPo);
+    }
+}
